e682e0283e1de6096757a23c583a32a6c1b9f3c0
bonnie199311 <bonnie111282@gmail.com>
2021-02-07 16:07:49 +0800

    添加ＲＷＤ及頁面連結

--- main.js
+++ main.js
@@ -6,5 +6,29 @@ $("#banner").slick({
 
 $("#works").slick({
     slidesToShow:3,
-    slidesToScroll:2
+    slidesToScroll:2,
+
+    responsive: [
+         // tablet
+        // under 1025 size what will do is...?
+        {
+            breakpoint: 1025,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+
+        // phone
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+
+    ]
+
+
 });
